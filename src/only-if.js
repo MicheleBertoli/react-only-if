784b40471f9e42810dcc3194164610cbda8fb0f6
Frederik Ring <frederik.ring@gmail.com>
2016-08-21 19:32:02 +0200

    enable passing of stateless components

--- src/only-if.js
+++ src/only-if.js
@@ -1,12 +1,26 @@
 import React from 'react';
 
 export default (Target, condition, Placeholder) => {
+  const renderedPlaceholder = Placeholder
+    ? React.createElement(Placeholder)
+    : null;
+
+  if (!Target.prototype.isReactComponent) {
+    const StatelessComponent = (props, context) => (
+      condition(props, context)
+        ? React.createElement(Target, props)
+        : renderedPlaceholder
+    );
+    StatelessComponent.contextTypes = Target.contextTypes;
+    return StatelessComponent;
+  }
+
   class Component extends Target {
     render() {
       if (condition(this.props, this.state, this.context)) {
         return super.render();
       }
-      return Placeholder ? React.createElement(Placeholder) : null;
+      return renderedPlaceholder;
     }
   }
   return Component;
--- src/only-if.spec.js
+++ src/only-if.spec.js
@@ -13,49 +13,95 @@ const Dummy = React.createClass({
   },
 });
 
+const StatelessDummy = () => (
+  <div>Stateless Dummy</div>
+);
+StatelessDummy.contextTypes = {
+  test: React.PropTypes.bool,
+};
+
 const Placeholder = () => <div>Placeholder</div>;
 
 describe('onlyIf', () => {
-  it('calls the condition function with the props', () => {
-    const callback = sinon.spy();
-    const props = { test: true };
-    const DummyOnlyIf = onlyIf(Dummy, callback);
-    shallow(<DummyOnlyIf {...props} />);
-    assert(callback.calledWith(props));
+  context('stateful components', () => {
+    it('calls the condition function with the props', () => {
+      const callback = sinon.spy();
+      const props = { test: true };
+      const DummyOnlyIf = onlyIf(Dummy, callback);
+      shallow(<DummyOnlyIf {...props} />);
+      assert(callback.calledWith(props));
+    });
+
+    it('calls the condition function with the state', () => {
+      const callback = sinon.spy();
+      const state = { test: true };
+      const DummyOnlyIf = onlyIf(Dummy, callback);
+      const wrapper = shallow(<DummyOnlyIf />);
+      wrapper.setState(state);
+      assert(callback.calledWith({}, state));
+    });
+
+    it('calls the condition function with the context', () => {
+      const callback = sinon.spy();
+      const context = { test: true };
+      const DummyOnlyIf = onlyIf(Dummy, callback);
+      shallow(<DummyOnlyIf />, { context });
+      assert(callback.calledWith({}, null, context));
+    });
+
+    it('renders the component if the condition is true', () => {
+      const DummyOnlyIf = onlyIf(Dummy, () => true);
+      const wrapper = shallow(<DummyOnlyIf />);
+      assert(wrapper.containsMatchingElement(Dummy));
+    });
+
+    it('renders the placeholder if the condition is false', () => {
+      const DummyOnlyIf = onlyIf(Dummy, () => false, Placeholder);
+      const wrapper = shallow(<DummyOnlyIf />);
+      assert(wrapper.contains(<Placeholder />));
+    });
+
+    it('renders null if the placeholder does not exist', () => {
+      const DummyOnlyIf = onlyIf(Dummy, () => false);
+      const wrapper = shallow(<DummyOnlyIf />);
+      assert.equal(wrapper.type(), null);
+    });
   });
 
-  it('calls the condition function with the state', () => {
-    const callback = sinon.spy();
-    const state = { test: true };
-    const DummyOnlyIf = onlyIf(Dummy, callback);
-    const wrapper = shallow(<DummyOnlyIf />);
-    wrapper.setState(state);
-    assert(callback.calledWith({}, state));
-  });
+  context('stateless components', () => {
+    it('calls the condition function with the props', () => {
+      const callback = sinon.spy();
+      const props = { test: true };
+      const DummyOnlyIf = onlyIf(StatelessDummy, callback);
+      shallow(<DummyOnlyIf {...props} />);
+      assert(callback.calledWith(props));
+    });
 
-  it('calls the condition function with the context', () => {
-    const callback = sinon.spy();
-    const context = { test: true };
-    const DummyOnlyIf = onlyIf(Dummy, callback);
-    shallow(<DummyOnlyIf />, { context });
-    assert(callback.calledWith({}, null, context));
-  });
+    it('calls the condition function with the context', () => {
+      const callback = sinon.spy();
+      const context = { test: true };
+      const DummyOnlyIf = onlyIf(StatelessDummy, callback);
+      shallow(<DummyOnlyIf />, { context });
+      assert(callback.calledOnce);
+      assert(callback.calledWith({}, context));
+    });
 
-  it('renders the component if the condition is true', () => {
-    const DummyOnlyIf = onlyIf(Dummy, () => true);
-    const wrapper = shallow(<DummyOnlyIf />);
-    assert(wrapper.containsMatchingElement(Dummy));
-  });
+    it('renders the component if the condition is true', () => {
+      const DummyOnlyIf = onlyIf(StatelessDummy, () => true);
+      const wrapper = shallow(<DummyOnlyIf />);
+      assert(wrapper.contains(<StatelessDummy />));
+    });
 
-  it('renders the placeholder if the condition is false', () => {
-    const DummyOnlyIf = onlyIf(Dummy, () => false, Placeholder);
-    const wrapper = shallow(<DummyOnlyIf />);
-    assert(wrapper.contains(<Placeholder />));
-  });
+    it('renders the placeholder if the condition is false', () => {
+      const DummyOnlyIf = onlyIf(StatelessDummy, () => false, Placeholder);
+      const wrapper = shallow(<DummyOnlyIf />);
+      assert(wrapper.contains(<Placeholder />));
+    });
 
-  it('renders null if the placeholder does not exist', () => {
-    const DummyOnlyIf = onlyIf(Dummy, () => false);
-    const wrapper = shallow(<DummyOnlyIf />);
-    assert.equal(wrapper.type(), null);
+    it('renders null if the placeholder does not exist', () => {
+      const DummyOnlyIf = onlyIf(StatelessDummy, () => false);
+      const wrapper = shallow(<DummyOnlyIf />);
+      assert.equal(wrapper.type(), null);
+    });
   });
 });
